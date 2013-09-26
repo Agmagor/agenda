4ae7b9ece7b05f2983d1d6651ff6143407d607a8
Agmagor <Agmagor@agmagor.be>
2013-09-26 17:37:28 +0200

    Router regexp compliant, passage de parametre, catch-all .htaccess

--- php/classes/Router.php
+++ php/classes/Router.php
@@ -8,18 +8,37 @@ class Router
     public $routes = array( //Rewrite Rules
         'info' => 'InfoController',
         'poney' => 'PoneyController',
+        '#info/[0-9]+#' => 'InfoController',
     );
     
-    public function dispatch($rule)
+    public function dispatch($url)
     {
-        if (isset($rule) && isset($this->routes[$rule]))
-            $controller = $this->routes[$rule];
+        if (!isset($url)) die;
+        $params = explode("/", $url);
+        if (isset($this->routes[$url]))
+            $controller = $this->routes[$url];
         else
-            $controller = $this->notfound_controller;
+        {
+            $match = $this->match_rule($url);
+            if ($match)
+            {
+                $controller = $match[0];
+                $params = $match[1];
+            }
+            else
+                $controller = $this->notfound_controller;
+        }
         
         include_once(_CONTROLLERS_DIR_.'/'.$controller.".php");
         $class = new $controller();
-        $class->run();
+        $class->run($params);
+    }
+    
+    public function match_rule($url) {
+        foreach($this->routes as $key => $route)
+            if (preg_match($key, $url))
+                return array($route, explode('/', $url));
+        return false;
     }
     
     public static function getInstance()
--- php/index.php
+++ php/index.php
@@ -1,10 +1,10 @@
 <?php
 include('./config/init.php'); //Init
-if (!isset($_GET['a']))
-    $_GET['a'] = 'index';
+if (!isset($_GET['url']))
+    $_GET['url'] = 'index';
 
 $smarty->display(_TPL_DIR_ . 'header.tpl');
 
-Router::getInstance()->dispatch($_GET['a']);
+Router::getInstance()->dispatch($_GET['url']);
 
 $smarty->display(_TPL_DIR_ . 'footer.tpl');
