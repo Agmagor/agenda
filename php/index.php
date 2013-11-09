0148e1489c668d1cf9dc9d5ae3ae617dba355029
Agmagor <Agmagor@agmagor.be>
2013-11-09 13:32:09 +0100

    prevent html display in api

--- php/index.php
+++ php/index.php
@@ -3,8 +3,12 @@ include('./config/init.php'); //Init
 if (!isset($_GET['url']))
     $_GET['url'] = 'index';
 
-$smarty->display(_TPL_DIR_ . 'header.tpl');
+$a = explode('/',$_GET['url']);
+
+if ($a[0] != "api")
+    $smarty->display(_TPL_DIR_ . 'header.tpl');
 
 Router::getInstance()->dispatch($_GET['url']);
 
-$smarty->display(_TPL_DIR_ . 'footer.tpl');
+if ($a[0] != "api")
+    $smarty->display(_TPL_DIR_ . 'footer.tpl');
